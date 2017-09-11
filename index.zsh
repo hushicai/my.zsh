5219ede00fb3473014858c3de82400a9ecd8a3ab
hushicai <hushicai@gf.com.cn>
2017-09-11 09:24:31 +0800

    update

--- index.zsh
+++ index.zsh
@@ -9,3 +9,12 @@ alias enwhistle='export http_proxy=http://127.0.0.1:8899 https_proxy=$http_proxy
 alias unwhistle='unset http_proxy https_proxy'
 
 export PYTHONSTARTUP=$HOME/my.zsh/python/startup.py
+
+export ANDROID_HOME=/Users/hushicai/Library/Android/sdk
+
+export GRADLE_HOME=/Users/hushicai/gradle-3.5-rc-3
+
+
+export PATH=$GRADLE_HOME/bin:$ANDROID_HOME/platform-tools:$PATH
+
+export SSLKEYLOGFILE=~/tls/sslkeylog.log
